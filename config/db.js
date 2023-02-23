058c186e30ed4e7f267bbae0c089f7a2747fc3e7
pawel-borkar <pawel.borkar@konverge.ai>
2023-02-23 18:21:01 +0530

    Add: mongoose and connect to mongodb

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+import mongoose from 'mongoose';
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(
+    `MongoDB connected: ${chalk.yellowBright.bold(conn.connection.host)}`
+  );
+};
+
+export default connectDB;
