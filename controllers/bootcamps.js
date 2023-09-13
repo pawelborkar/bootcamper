efc5b92e068a42299c5352c2687d762d6b7c9dea
Pawel Borkar <pawel@duck.com>
2023-09-13 17:09:21 +0530

    'refactor: add async error handler'

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,6 +2,7 @@
 Controller: bootcamps
 */
 import ErrorResponse from '../utils/errorResponse.js';
+import asyncHandler from '../middleware/async.js';
 import Bootcamp from '../models/Bootcamp.js';
 /*
 @desc: Get all bootcamps information
@@ -9,16 +10,12 @@ import Bootcamp from '../models/Bootcamp.js';
 @route: GET /api/v1/bootcamps
 @access: Public
 */
-const getAllBootcamps = async (req, res) => {
-  try {
-    const allBootcamps = await Bootcamp.find();
-    res
-      .status(200)
-      .json({ success: true, count: allBootcamps.length, data: allBootcamps });
-  } catch (error) {
-    res.status(400).json({ success: false });
-  }
-};
+const getAllBootcamps = asyncHandler(async (req, res, next) => {
+  const allBootcamps = await Bootcamp.find();
+  res
+    .status(200)
+    .json({ success: true, count: allBootcamps.length, data: allBootcamps });
+});
 
 /*
 @desc: Get all bootcamps information
@@ -26,29 +23,19 @@ const getAllBootcamps = async (req, res) => {
 @route: GET /api/v1/bootcamps/:id
 @access: Public
 */
-const getSingleBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findById(req.params.id);
+const getSingleBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(
-          `Bootcamp not found with an id of ${req.params.id}`,
-          404
-        )
-      );
-    }
-    res.status(200).json({ success: true, data: bootcamp });
-  } catch (error) {
-    // res.status(400).json({ success: false });
-    next(
+  if (!bootcamp) {
+    return next(
       new ErrorResponse(
         `Bootcamp not found with an id of ${req.params.id}`,
         404
       )
     );
   }
-};
+  res.status(200).json({ success: true, data: bootcamp });
+});
 
 /*
 @desc: Add a new bootcamp
@@ -56,20 +43,13 @@ const getSingleBootcamp = async (req, res, next) => {
 @route: POST /api/v1/bootcamp
 @access: Private
 */
-const createBootcamp = async (req, res) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (egetSingleBootcamprror) {
-    res.status(400).json({
-      success: false,
-      message: 'Invalid Input',
-    });
-  }
-};
+const createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 /*
 @desc: Update a bootcamp
@@ -77,18 +57,23 @@ const createBootcamp = async (req, res) => {
 @route: PUT /api/v1/bootcamp/:id
 @access: Private
 */
-const updateBootcamp = async (req, res) => {
+const updateBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
 
   if (!bootcamp) {
-    return res.status(400).json({ success: false });
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with an id of ${req.params.id}`,
+        404
+      )
+    );
   }
 
   res.status(200).json({ success: true, data: bootcamp });
-};
+});
 
 /*
 @desc: Delete a bootcamp from catalogue
@@ -96,15 +81,20 @@ const updateBootcamp = async (req, res) => {
 @route: DELETE /api/v1/bootcamp/:id
 @access: Private
 */
-const deleteBootcamp = async (req, res) => {
+const deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
   if (!bootcamp) {
-    return res.status(400).json({ success: false });
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with an id of ${req.params.id}`,
+        404
+      )
+    );
   }
 
   res.status(200).json({ success: true, data: bootcamp });
-};
+});
 
 export {
   getAllBootcamps,
--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,32 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+import ErrorResponse from '../utils/errorResponse.js';
 
-  res.status(err.statusCode || 500).json({
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+  console.log(err);
+
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Bootcamp not found with an id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Bootcamp name already exists.';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongooose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'Internal Server Error',
+    error: error.message || 'Internal Server Error',
   });
 };
 
