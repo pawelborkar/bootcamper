41da2f8ff53c09ee397ff827b66d90eaee17d141
Pawel Borkar <pawel@duck.com>
2023-09-14 12:28:58 +0530

    fix: mongoose warning

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 import mongoose from 'mongoose';
 const connectDB = async () => {
+  mongoose.set('strictQuery', false);
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     // useCreateIndex: true,
