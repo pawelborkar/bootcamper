bda7a7e34f69c497ce9e30ba0c48a3c500c82a8e
Pawel Borkar <pawel@duck.com>
2023-09-14 18:54:02 +0530

    feature: add database seeder

---
+++ seeder.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { mongoose } from 'mongoose';
+import dotenv from 'dotenv';
+import Bootcamp from './models/Bootcamp.js';
+import chalk from 'chalk';
+
+dotenv.config({ path: './config/config.env' });
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+// Connect to mongodb database
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// Read data from the file
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+
+// Import data from the file
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log(chalk.bgGreenBright('Data imported successfully...'));
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete data from the file
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log(chalk.bgRedBright('Data deleted successfully...'));
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Reading flags from the terminal and calling functions accordingly
+
+if (process.argv[2] === 'import' || process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === 'delete' || process.argv[2] === '-d') {
+  deleteData();
+}
--- utils/geocoder.js
+++ utils/geocoder.js
@@ -3,13 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './config/config.env' });
 
 const options = {
-    provider : process.env.GEOCODER_PROVIDER,
-    httpAdapter : 'https',
-    apiKey : process.env.GEOCODER_APIKEY,
-    formatter : null
-}
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_APIKEY,
+  formatter: null,
+};
 
-
-const geocoder = node_geocoder(options)
+const geocoder = node_geocoder(options);
 
 export default geocoder;
