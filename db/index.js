28ce646dae7cef72ebca4b4060de2f1c7d6bcaf0
Pawel Borkar <pawel@duck.com>
2024-12-14 18:12:15 +0530

    chore: refactoring

--- db/index.js
+++ db/index.js
@@ -1,17 +1,12 @@
-import { connect, set } from 'mongoose';
+import { connect, set } from "mongoose";
 const connectDB = async () => {
   try {
-    set('strictQuery', false);
-    const conn = await connect(
-      `${process.env.MONGO_URI}/${process.env.DB_NAME}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log(
-      `MongoDB connected: ${conn.connection.host}`
-    );
+    set("strictQuery", false);
+    const conn = connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`DB Error: ${error}`);
     process.exit(1);
--- middleware/error.js
+++ middleware/error.js
@@ -1,13 +1,13 @@
-import ErrorResponse from '../utils/errorResponse.js';
+import ErrorResponse from "../utils/errorResponse.js";
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, _req, res, _next) => {
   let error = { ...err };
 
   error.message = err.message;
   console.log(err);
 
   // Mongoose bad ObjectId
-  if (err.name === 'CastError') {
+  if (err.name === "CastError") {
     const message = `Resource not found.`;
     error = new ErrorResponse(message, 404);
   }
@@ -19,14 +19,14 @@ const errorHandler = (err, req, res, next) => {
   }
 
   // Mongooose validation error
-  if (err.name === 'ValidationError') {
+  if (err.name === "ValidationError") {
     const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || 'Internal Server Error',
+    error: error.message || "Internal Server Error",
   });
 };
 
